009ae1a88b0a24d9bc9871c0b9b85e51fce08a17
Cecilia Rossi <cecilia.rossi.3@studenti.unipd.it>
2025-02-17 16:43:30 +0100

    fixed the initialization of the membrane potential

--- Neuron and Synapse Models/ring_attractor.py
+++ Neuron and Synapse Models/ring_attractor.py
@@ -8,17 +8,39 @@ def neuron_distance(i ,j, N=120):
 def return_radians_angle(i, N=120):
     return 2*pi*i/N * radian
 
+def visualise_connectivity(S):
+    Ns = len(S.source)
+    Nt = len(S.target)
+    figure(figsize=(10, 4))
+    subplot(121)
+    plot(zeros(Ns), arange(Ns), 'ok', ms=10)
+    plot(ones(Nt), arange(Nt), 'ok', ms=10)
+    for i, j in zip(S.i, S.j):
+        plot([0, 1], [i, j], '-k')
+    xticks([0, 1], ['Source', 'Target'])
+    ylabel('Neuron index')
+    xlim(-0.1, 1.1)
+    ylim(-1, max(Ns, Nt))
+    subplot(122)
+    plot(S.i, S.j, 'ok')
+    xlim(-1, Ns)
+    ylim(-1, Nt)
+    xlabel('Source neuron index')
+    ylabel('Target neuron index')
+
 
 class RingAttractor():
-    def __init__(self, N =120, Vth=-48 * mV, V_rest=-70 *mV, V_reset=-80 *mV, wee = 46*mV, wei = 46*mV, wie = 420 *mV, wii = 560 *mV, sigma=0.4, mode_weights="gaussian"):
+    def __init__(self, N =120, Vth=-48 * mV, V_rest=-70 *mV, V_reset=-80 *mV, wee = 1000*mV, wei = 5*mV, wie = 500 *mV, wii = 4 *mV, sigma=0.4, mode_weights="gaussian"):
         #self.Vthr = Vth * mV
         #self.V_reset = V_reset * mV
         #self.V_rest = V_rest * mV
         self.N = N
         self.sigma = sigma
 
-        self.inhibit_neuron = NeuronGroup(1, LIF_eq, threshold = "V > Vth", reset = "V = V_reset", method="euler", name="inhibitiry_neurons", dt=2*ms)
-        self.excitatory_neurons = NeuronGroup(self.N, LIF_eq, threshold = "V > Vth", reset = "V = V_reset", method="euler", name="excitatory_neurons", dt=2*ms)
+        self.inhibit_neuron = NeuronGroup(1, LIF_eq, threshold = "V > Vth", reset = "V = V_reset", method="euler", name="inhibitory_neurons")
+        self.excitatory_neurons = NeuronGroup(self.N, LIF_eq, threshold = "V > Vth", reset = "V = V_reset", method="euler", name="excitatory_neurons")
+        self.inhibit_neuron.V = V_rest
+        self.excitatory_neurons.V = V_rest
 
         rows, cols = np.indices((self.N, self.N))
         distance_matrix = neuron_distance(rows, cols, self.N)
@@ -28,26 +50,13 @@ class RingAttractor():
         elif mode_weights == "cosine":
             self.weights_matrix = "wee * cos(return_radians_angle(i) - return_radians_angle(j))"
  
-        self.Ring2Inh = Synapses(self.excitatory_neurons, self.inhibit_neuron, "W_ring2inh : volt", name="ring2inh_synapses", dt=2*ms,
-                                 on_pre= """ 
-                                 V_post = W_ring2inh 
-                                 """
-                                 )
+        self.Ring2Inh = Synapses(self.excitatory_neurons, self.inhibit_neuron, "W_ring2inh : volt", name="ring2inh_synapses", on_pre="V_post += W_ring2inh")
 
-        self.Inh2Ring = Synapses(self.inhibit_neuron, self.excitatory_neurons, model="W_inh2ring : volt", name="inh2ring_synapses",dt=2*ms, 
-                                  on_pre="""
-                                  V_post = W_inh2ring
-                                  """
-                                  )
+        self.Inh2Ring = Synapses(self.inhibit_neuron, self.excitatory_neurons, model="W_inh2ring : volt", name="inh2ring_synapses", on_post="V_post -= W_inh2ring ") #in this case I want the inhibitory neuron (presynaptic) to inhibit the neuron only if the post synaptic (excitatory) is firing too much
  
-        self.Inh2Inh = Synapses(self.inhibit_neuron, self.inhibit_neuron, model = "W_inh2inh : volt", name="inh2inh_synapses", dt=2*ms,
-                                on_pre="""
-                                V_post = W_inh2inh
-                                """
-                                )
-
-        self.Ring2Ring = Synapses(self.excitatory_neurons, self.excitatory_neurons, model="W_ring2ring : volt", name="ring2rings_synapses",dt=2*ms,
-                                  )
+        self.Inh2Inh = Synapses(self.inhibit_neuron, self.inhibit_neuron, model = "W_inh2inh : volt", name="inh2inh_synapses", on_post="V_post -= W_inh2inh")
+        
+        self.Ring2Ring = Synapses(self.excitatory_neurons, self.excitatory_neurons, model="W_ring2ring : volt", name="ring2rings_synapses", on_pre="V_post += W_ring2ring")
 
         self.Ring2Inh.connect()
         self.Inh2Ring.connect()
@@ -56,9 +65,15 @@ class RingAttractor():
         self.Ring2Inh.W_ring2inh = wei
         self.Inh2Ring.W_inh2ring = wie
         self.Inh2Inh.W_inh2inh = wii
-        self.Ring2Ring.W_ring2ring[:] = self.weights_matrix.flatten() 
-        self.inhibit_neuron.V = V_rest
+        self.Ring2Ring.W_ring2ring[:] = self.weights_matrix.flatten()
         #visualise_connectivity(self.Ring2Inh, self.Inh2Ring, self.Inh2Inh, self.Ring2Ring)
+
+        #Plotting the connectivity between neurons
+        visualise_connectivity(self.Ring2Inh)
+        visualise_connectivity(self.Inh2Ring)
+        visualise_connectivity(self.Inh2Inh)
+        visualise_connectivity(self.Ring2Ring)
+        
 
         self.net = Network(collect()) #level=1, [self.excitatory_neurons, self.inhibit_neuron, self.Ring2Inh, self.Ring2Ring, self.Inh2Inh, self.Inh2Ring])) #it`s not collecting anything
         self.net.add([self.excitatory_neurons, self.inhibit_neuron, self.Ring2Inh, self.Ring2Ring, self.Inh2Inh, self.Inh2Ring])
