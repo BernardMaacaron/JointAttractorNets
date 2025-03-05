bef567b7eb867c8b060e7bce44239f2665ccf987
Bernard Maacaron <bernard_maacaron@hotmail.com>
2025-03-05 14:38:23 +0100

    Minor adjustments to core functions in library

--- Tools/gaussianGenerator.py
+++ Tools/gaussianGenerator.py
@@ -25,7 +25,7 @@ class Gaussian_Input_Generator:
                  sigma=1,
                  noise=False,
                  normalised=True,
-                 verbose=False):
+                 verbose=0):
         
         self.n=n
         self.m=amp  #amplitude
@@ -35,15 +35,18 @@ class Gaussian_Input_Generator:
         self.normalised=normalised
         self.x=np.linspace(0, 360, self.n, endpoint=True)
         self.y=None
-        self.verbose=verbose        
-    
+        self.verbose=verbose
+        
+        self.adjusted_mu = self.mu % 360
+        self.ground_truth_index = int((self.adjusted_mu/360)*self.n)      # Find the ground truth neuron index        
+
     def generate_cue(self):
     
         # Create a wrap based on the number of neurons
         wraps = np.arange(-self.n/2, self.n/2+1)
         gaussianComponent = np.exp(-0.5 * ((self.x[:,np.newaxis]-self.mu+wraps*360) /self.sigma)**2)
         wrappedGaussian = self.m * np.sum(gaussianComponent, axis=1)
-        normalizationTerm=1/(self.sigma*((2*pi)**0.5))
+        normalizationTerm=(self.sigma*((2*pi)**0.5))
 
         y=wrappedGaussian
         
@@ -51,19 +54,19 @@ class Gaussian_Input_Generator:
             y=self.add_noise(y)
             
         if self.normalised:
-            y=y*normalizationTerm
+            y=y/normalizationTerm
         
-        if self.verbose:
+        if self.verbose==1:
+            print('Theoretical Angle:', self.adjusted_mu,'\tTheoretical Neuron Index:', self.ground_truth_index,
+                  '\n Estimated Angle:', self.x[np.argmax(y)], '\t Estimated Neuron:', np.argmax(y), end='\n')
+        if self.verbose==2:
+            print('Theoretical Angle:', self.adjusted_mu,'\tTheoretical Neuron Index:', self.ground_truth_index,
+                  '\nEstimated Angle:', self.x[np.argmax(y)], '\t Estimated Neuron Index:', np.argmax(y), end='\n')
             for index, (angle, value) in enumerate(zip(self.x, y)):
                 print('Gaussian', index, 'at', angle, 'has value', value)
-            
-            print('The angle should theoretically be', self.mu,
-                '.\nThe peak is at', self.x[np.argmax(y)],
-                '. The respective neuron is', np.argmax(y))
-            
+        
         self.y = y
         return y
-    
     
     def plot_gaussians_polar(self):
         # Generate the wrapped Gaussian values
@@ -72,18 +75,25 @@ class Gaussian_Input_Generator:
         plot_on_circle(self.x, gaussian_values, title='Wrapped Gaussian on a Circle',
                        legend_label='Wrapped Gaussian', legend_kwargs={'loc': 'upper right'})
 
-
     def add_noise(self,I):
         I_noisy=I+normal(0,1,len(self.x))*2e-2
         return I_noisy
     
-    def to_brian2_neuron_group(self):
-        input_gen = NeuronGroup(self.n, 'I : 1', threshold='I > 0', reset='I = 0', method='exact')
-        if self.y is None:
-            input_gen.I = self.generate_cue()
+    def peak_position(self):
+        return self.x[np.argmax(self.y)]
+    
+    def to_brian2_neuron_group(self, peak_only=False):
+        if peak_only:
+            input_gen = NeuronGroup(1, 'I : 1', threshold='I > 0', reset='I = 0', method='exact')
+            input_gen.I = 1
+            return input_gen, self.ground_truth_index
         else:
-            input_gen.I = self.y
-        return input_gen
+            input_gen = NeuronGroup(self.n, 'I : 1', threshold='I > 0', reset='I = 0', method='exact')
+            if self.y is None:
+                input_gen.I = self.generate_cue()
+            else:
+                input_gen.I = self.y
+            return input_gen
 
 # +
 # num_neurons = 20
